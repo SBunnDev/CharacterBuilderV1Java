1c67c5ee395215342da0e2a5b4fb897297adba02
SBunnDev <87293052+SBunnDev@users.noreply.github.com>
2021-10-18 23:45:33 -0400

    Dealt with rounding issue for negative numbers

--- src/Main.java
+++ src/Main.java
@@ -13,38 +13,52 @@ public class Main {
         int intl;
         int cha;
 
+
         //TODO: Fix negative number modifier math
         //Statblock generation TODO: Store these in array , also store roll values and ask player what value to bestow where, show what values are left
 
         //Strength
         System.out.println("Enter your strength >> ");
         str = input.nextInt();
-        int strMod=(str-10)/2;
+        int strMod= ((str-10)/2);
+        //If statement required for issues with Math.floor not working as expected for negative numbers
+            if (str < 10)
+                strMod=(str-11)/2;
 
         //Dexterity
         System.out.println("Enter your dexterity >> ");
         dex = input.nextInt();
         int dexMod=(dex-10)/2;
+            if (dex < 10)
+                dexMod=(dex-11)/2;
 
         //Constitution
         System.out.println("Enter your constitution >> ");
         con = input.nextInt();
         int conMod=(con-10)/2;
+        if (con < 10)
+            conMod=(con-11)/2;
 
         //Wisdom
         System.out.println("Enter your wisdom >> ");
         wis = input.nextInt();
         int wisMod=(wis-10)/2;
+        if (wis < 10)
+            wisMod=(wis-11)/2;
 
         //Intelligence
         System.out.println("Enter your intelligence >> ");
         intl = input.nextInt();
         int intlMod=(intl-10)/2;
+            if (intl < 10)
+                intlMod=(intl-11)/2;
 
         //Charisma
         System.out.println("Enter your charisma >> ");
         cha = input.nextInt();
         int chaMod=(cha-10)/2;
+            if (cha < 10)
+                chaMod=(cha-11)/2;
 
         //Proficiency
         System.out.println("Enter your proficiency bonus >> ");
